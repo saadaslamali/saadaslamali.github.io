3cfad2575e8be232cb5ccef4014eda74719d1d50
saadaslamali <149980567+saadaslamali@users.noreply.github.com>
2023-12-11 18:07:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ duration:0,
   }
 })
 
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    window.onload = function () {
-      gsap.to(".pagecontainer", {
+     gsap.to(".pagecontainer", {
   x: 0,
   opacity:1,
   ease: "slow(0.7,0.7,false)",
@@ -39,8 +36,26 @@ duration:1,
     // snap:".r0",
   }
 })
-    };
-});
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     window.onload = function () {
+//       gsap.to(".pagecontainer", {
+//   x: 0,
+//   opacity:1,
+//   ease: "slow(0.7,0.7,false)",
+// duration:1,
+//   scrollTrigger: {
+//     trigger: ".pagecontainer",
+//     start: "top 1000px",
+//     end: "center center",
+//     scrub: false,
+//     markers: false,
+//     id: "scrub",
+//     // snap:".r0",
+//   }
+// })
+//     };
+// });
 
 
 for (let i = 0; i < links.length; i++) {
